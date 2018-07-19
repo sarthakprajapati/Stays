c916fe808d09858c71d0d859e61d0b490d2bffad
Krishanu Mallik Thakur <mkrishanu98@gmail.com>
2018-07-19 20:19:50 +0530

    hotel added

---
+++ hotel/include/footer.php
@@ -0,0 +1,16 @@
+<footer class="tm-black-bg">
+		<div class="container">
+			<div class="row">
+				<p class="tm-copyright-text">Copyright &copy; 2084 Your Company Name 
+                
+                | Designed by <a rel="nofollow" href="http://www.templatemo.com" target="_parent">templatemo</a></p>
+			</div>
+		</div>		
+	</footer>
+	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>      		<!-- jQuery -->
+  	<script type="text/javascript" src="js/bootstrap.min.js"></script>					<!-- bootstrap js -->
+  	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>			<!-- flexslider js -->
+  	<script type="text/javascript" src="js/templatemo-script.js"></script>      		<!-- Templatemo Script -->
+	<script src="js/include/<?php echo $filename;?>.js"></script>
+ </body>
+ </html>
