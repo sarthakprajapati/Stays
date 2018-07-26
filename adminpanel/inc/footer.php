7eddd9ddae4dea58b93f1ac7c38d49a6414abbe0
Krishanu Mallik Thakur <mkrishanu98@gmail.com>
2018-07-26 23:53:19 +0530

    r

--- adminpanel/inc/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="copyright">
-            <p>Copyright © 2018. All rights reserved.</p>
-        </div>
-    </div>
-</div>
-</div>
--- adminpanel/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-ob_start();
-session_start();
-header("location: ../hotel/index.php");
-session_destroy();
- ?>
